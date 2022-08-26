99b8eff0e92e28bb21f34c58dbbfa1f675b05b97
Isaac Pikunic <ipikunic21@gmail.com>
2022-08-27 00:22:11 +1000

    Air done

--- globes.js
+++ globes.js
@@ -6,13 +6,13 @@ fetch('./data/globe/ne_110m_admin_0_countries.geojson').then(res => res.json()).
     {
         // air pollution exposure value
         function getAQValue(d) {
-            let content = `**DATA FOR ${d.ADMIN} (${d.ISO_A3}) IS NOT AVAILABLE.`;
+            let content = `<b>DATA FOR ${d.ADMIN} (${d.ISO_A3}) IS NOT AVAILABLE.</b>`;
 
             data.forEach(index => {
                 if (index.CODE == d.ISO_A3)
                     content = `<b>${d.ADMIN} (${d.ISO_A3}):<br>Exposure to PM2.5: ${index.VALUE} mg/m<sup>3</sup></b>`;
             });
-            
+
             return content;
         } 
 
