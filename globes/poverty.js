d257b7e8d67e90e5877087c9b1e2d2634ea1f1c8
Anti <spiralknightsowner@gmail.com>
2022-08-27 14:51:51 +1000

    About to try diff globe

--- globes/poverty.js
+++ globes/poverty.js
@@ -1,4 +1,4 @@
-fetch('../datasets/ne_110m_admin_0_countries.geojson').then(res => res.json()).then(countries =>
+fetch('/data/globe/ne_110m_admin_0_countries.geojson').then(res => res.json()).then(countries =>
     {
       const world = Globe()
       (document.getElementById('globeViz'))
@@ -13,11 +13,11 @@ fetch('../datasets/ne_110m_admin_0_countries.geojson').then(res => res.json()).t
           Population: <i>${d.POP_EST}</i>
         `)
         (document.getElementById('globeViz'))
-    });    fetch('../datasets/ne_110m_admin_0_countries.geojson').then(res => res.json()).then(countries =>
+    });    fetch('/data/globe/ne_110m_admin_0_countries.geojson').then(res => res.json()).then(countries =>
     {
       const world = Globe()
         .globeImageUrl('//unpkg.com/three-globe/example/img/earth-dark.jpg')
-        .hexPolygonsData(countries.features)
+        .hexPolygonsData(countries.features) // features contains type, properties, bbox, geometry
         .hexPolygonResolution(3)
         .hexPolygonMargin(0.3)
         .hexPolygonColor(() => `#${Math.round(Math.random() * Math.pow(2, 24)).toString(16).padStart(6, '0')}`)
@@ -26,3 +26,12 @@ fetch('../datasets/ne_110m_admin_0_countries.geojson').then(res => res.json()).t
           Population: <i>${d.POP_EST}</i>
         `)
     });
+
+
+
+    // live api stuff
+fetch('https://api.worldbank.org/pip/v1/pip?country=all&year=2018&povline=1.9&fill_gaps=false')
+.then(res => res.json())
+.then(t => console.log(t[0])) // think we just throw t into the fkin polgygonlayer.
+
+// .then(t => console.log(t[0].region_name))
