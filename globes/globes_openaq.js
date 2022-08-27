f9e4555528dc396d07f1a22c2e180336e48a382d
Anti <spiralknightsowner@gmail.com>
2022-08-27 20:57:10 +1000

    Hexbindata is done.

--- globes/globes_openaq.js
+++ globes/globes_openaq.js
@@ -3,18 +3,44 @@ const world = Globe()(document.getElementById('globeViz'))
       .backgroundImageUrl('//unpkg.com/three-globe/example/img/night-sky.png');
 const options = {method: 'GET', headers: {Accept: 'application/json'}};
 
-fetch('https://api.openaq.org/v2/locations?limit=100&page=1&offset=0&sort=desc&radius=1000&order_by=lastUpdated&dumpRaw=false', options)
+const weightColor = d3.scaleLinear()
+.domain([0, 60])
+.range(['yellow', 'darkred'])
+.clamp(true);
+
+fetch('https://api.openaq.org/v2/locations?limit=200&page=1&offset=0&sort=desc&radius=100&order_by=lastUpdated&dumpRaw=false', options)
   .then(response => response.json())
   .then(response => {
-    world.labelsData(response.results)
+    world.hexBinPointsData(response.results)
+    .hexBinPointLat(d => d.coordinates.latitude)
+    .hexBinPointLng(d => d.coordinates.longitude)
+    
+    // Label aesthetics
+    .hexLabel(d => d.name)
+    .hexBinPointWeight(d => d.parameters[0].average)
+    .hexAltitude(({ sumWeight }) => sumWeight * 0.0070)
+    .hexTopColor(d => weightColor(d.sumWeight))
+    .hexSideColor(d => weightColor(d.sumWeight))
+    .hexLabel(d => setLabel(d))
   });
 
 // World settings
-world.labelLat(d => d.coordinates.latitude)
-.labelLng(d => d.coordinates.longitude)
-.labelText(d => d.name)
-.labelSize(d => Math.sqrt(d.parameters[0].average) *0.3)
-.labelDotRadius(d => Math.sqrt(d.parameters[0].average) *0.3)
+world
+
+// Set label for hex points.
+function setLabel(d) {
+    label = `The name for this is not available.`
+
+    console.log(d.points[0].parameters)
+    console.log(d)
+    params = d.points[0].parameters
+    label = `${d.points[0].name}.
+    <br>
+    Contains ${params[0].average} ${params[0].unit} of ${params[0].displayName}`
+    return label;
+}
+// .labelSize(d => Math.sqrt(d.parameters[0].average))
+// .labelDotRadius(d => Math.sqrt(d.parameters[0].average) *0.3)
 
 
 // fetch('/data/globe/simple_cities.json').then(res => res.json()).then(places => {
