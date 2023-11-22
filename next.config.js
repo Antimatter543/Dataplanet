182aff08d932cd299c4501d9e5906c4b921f1214
unknown <110725093+shirei220@users.noreply.github.com>
2023-11-22 17:31:20 +1000

    updated config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 module.exports = {
     output: "export",
-    basePath: process.env.NODE_ENV === "production" ? "/Dataplanet": undefined,
-    experimental: {
-      appDir: true,
-    },
-    images: {
-      unoptimized: true,
-    },
-    reactStrictMode: true,
-  }
+    basePath: process.env.NODE_ENV === "production" ? "/Dataplanet" : undefined,
+    assetPrefix: process.env.NODE_ENV === "production" ? '/Dataplanet/' : undefined
+}
